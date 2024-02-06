5c518afb4827e6dfe2491931835b55215d2d006f
cosicmarko1 <cosicmarko1@gmail.com>
2024-02-07 00:37:25 +0100

    Implemented choosing time and dog size with buttons on LCD display

--- code/lcdSetup/lcdSetup.ino
+++ code/lcdSetup/lcdSetup.ino
@@ -17,90 +17,78 @@ int selectedDogSize = 0; // 0: Small Dog, 1: Medium Dog, 2: Big Dog
 void setup() {
   Serial.begin(115200);
 
-  lcd.begin();  // If your LCD library doesn't require parameters
+  lcd.begin();  
 
-  // Display "Enter time" message
   lcd.print("Enter time");
 
-  // Set up button pins with internal pull-up resistors
   pinMode(buttonUpPin, INPUT_PULLUP);
   pinMode(buttonDownPin, INPUT_PULLUP);
   pinMode(buttonOKPin, INPUT_PULLUP);
 }
 
 void loop() {
-  // Check for button presses and update time accordingly
   handleButtons();
 
-  // Display the current time and dog size on the LCD display
   printOnLCD();
 
-  // Wait for 1000 milliseconds
   delay(1000);
 
-  // If time is set and dog size is selected, update the clock
   if (setParametersState == 4) {
     updateTime();
   }
 }
 
 void handleButtons() {
-  // Check for "Up" button press
   if (digitalRead(buttonUpPin) == LOW) {
     switch (setParametersState) {
-      case 0: // Hours
+      case 0: 
         enteredHours++;
         break;
-      case 1: // Minutes
+      case 1: 
         enteredMinutes++;
         break;
-      case 2: // Seconds
+      case 2: 
         enteredSeconds++;
         break;
-      case 3: // Dog Size
+      case 3: 
         selectedDogSize++;
         break;
     }
-    //delay(50);  // Debounce delay
   }
 
-  // Check for "Down" button press
   if (digitalRead(buttonDownPin) == LOW) {
     switch (setParametersState) {
-      case 0: // Hours
+      case 0: 
         enteredHours--;
         break;
-      case 1: // Minutes
+      case 1: 
         enteredMinutes--;
         break;
-      case 2: // Seconds
+      case 2: 
         enteredSeconds--;
         break;
-      case 3: // Dog Size
+      case 3: 
         selectedDogSize--;
         break;
     }
-    //delay(50);  // Debounce delay
   }
 
-  // Check for "OK" button press
   if (digitalRead(buttonOKPin) == LOW) {
     updateState();
-    //delay(50);  // Debounce delay
   }
 }
 
 void incrementEnteredValue() {
   switch (setParametersState) {
-    case 0:  // Hours
+    case 0:  
       enteredHours++;
       if (enteredHours > 23) enteredHours = 0;
       break;
-    case 1:  // Minutes
+    case 1:  
       enteredMinutes++;
       if (enteredMinutes > 59) enteredMinutes = 0;
       break;
-    case 2:  // Seconds
+    case 2:  
       enteredSeconds++;
       if (enteredSeconds > 59) enteredSeconds = 0;
       break;
@@ -112,15 +100,15 @@ void incrementEnteredValue() {
 
 void decrementEnteredValue() {
   switch (setParametersState) {
-    case 0:  // Hours
+    case 0:  
       enteredHours--;
       if (enteredHours < 0) enteredHours = 23;
       break;
-    case 1:  // Minutes
+    case 1:  
       enteredMinutes--;
       if (enteredMinutes < 0) enteredMinutes = 59;
       break;
-    case 2:  // Seconds
+    case 2:  
       enteredSeconds--;
       if (enteredSeconds < 0) enteredSeconds = 59;
       break;
@@ -130,29 +118,11 @@ void decrementEnteredValue() {
   }
 }
 
-/*void incrementTimeValue(int* value) {
-  (*value)++;
-  if (*value > 59) *value = 0;
-}
-void decrementTimeValue(int* value) {
-  (*value)--;
-  if (*value < 0) *value = 59;
-}
-void incrementDogSize() {
-  selectedDogSize++;
-  if (selectedDogSize > 2) selectedDogSize = 0;
-}
-void decrementDogSize() {
-  selectedDogSize--;
-  if (selectedDogSize < 0) selectedDogSize = 2;
-} */
-
 void updateState() {
   setParametersState++;
 
   if (setParametersState > 3) {
     setParametersState = 4;  // Set to 4 to indicate that time setting and dog size selection are done
-    // Set the initial time with the entered values
     setTime(enteredHours, enteredMinutes, enteredSeconds, 1, 1, 2024);
   }
 }
@@ -160,7 +130,6 @@ void updateState() {
 void printOnLCD() {
   lcd.clear();
 
-  // Print the current state and value on the LCD display
   if (setParametersState <= 2) {
     lcd.print("Set ");
     if (setParametersState == 0) {
@@ -173,6 +142,7 @@ void printOnLCD() {
       lcd.print("seconds: ");
       lcd.print(enteredSeconds);
     }
+
   } else if (setParametersState == 3) {
     lcd.print("Select dog size:");
     lcd.setCursor(0, 1);
@@ -187,8 +157,8 @@ void printOnLCD() {
         lcd.print("Big (15-30kg)");
         break;
     }
+
   } else {
-    // Display the running clock
     lcd.print(hour());
     printDigits(minute());
     printDigits(second());
@@ -215,6 +185,5 @@ void printDigits(int digits) {
 }
 
 void updateTime() {
-  // Update the time based on the real-time clock
   adjustTime(0.5);
 }
